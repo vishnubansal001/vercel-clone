d84ca2c32b2652ff3763b877da4ae6ada7106831
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2024-03-01 23:28:14 +0530

    created the reverse proxy server

---
+++ s3-reverse-proxy/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+
+const app = express();
+const PORT = 8000;
+
+const BASE_PATH =
+  "https://vercel-clone-outputs.s3.ap-south-1.amazonaws.com/__outputs";
+
+const proxy = httpProxy.createProxy();
+
+app.use((req, res) => {
+  const hostname = req.hostname;
+  const subdomain = hostname.split(".")[0];
+
+  const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+
+proxy.on("proxyReq", (proxyReq, req, res) => {
+  const url = req.url;
+  if (url === "/") proxyReq.path += "index.html";
+});
+
+app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`));
